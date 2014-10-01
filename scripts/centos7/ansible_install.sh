2e7957e7554c86a633cce607f46bce17695e469d
Tamas Tarjanyi <tamas.dev.tarjanyi@gmail.com>
2014-10-01 19:18:33 +0200

    epel release package url update

--- scripts/centos7/ansible_install.sh
+++ scripts/centos7/ansible_install.sh
@@ -6,7 +6,7 @@ echo "==========================================================================
 echo " Started ansible_install.sh ..."
 echo "==============================================================================="
 echo "  Installing Ansible from EPEL repo..."
-yum -y install http://www.nic.funet.fi/pub/mirrors/fedora.redhat.com/pub/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
+yum -y install http://dl.fedoraproject.org/pub/epel/7/$(uname -m)/e/epel-release-7-2.noarch.rpm
 yum -y install ansible
 echo "localhost" >/etc/ansible/hosts
 echo "==============================================================================="
